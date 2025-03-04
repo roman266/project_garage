31497f5b823d6b575a8247efa005a307b132fb83
Andriy <81444060+DeepDF@users.noreply.github.com>
2025-03-04 17:18:42 +0200

    Feature/images to posts (#91)
    * images_to_posts
    
    * table with images
    
    * +
    
    * HomeController to json response
    
    * remove writlines
    
    * Update PostsController.cs
    
    * Update app.db

--- project_garage/Migrations/ApplicationDbContextModelSnapshot.cs
+++ project_garage/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -263,6 +263,26 @@ namespace project_garage.Migrations
                     b.ToTable("Messages");
                 });
 
+            modelBuilder.Entity("project_garage.Models.DbModels.PostImageModel", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("ImageUrl")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.Property<Guid>("PostId")
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("PostId");
+
+                    b.ToTable("PostImages");
+                });
+
             modelBuilder.Entity("project_garage.Models.DbModels.PostModel", b =>
                 {
                     b.Property<Guid>("Id")
@@ -549,6 +569,17 @@ namespace project_garage.Migrations
                     b.Navigation("Sender");
                 });
 
+            modelBuilder.Entity("project_garage.Models.DbModels.PostImageModel", b =>
+                {
+                    b.HasOne("project_garage.Models.DbModels.PostModel", "Post")
+                        .WithMany("Images")
+                        .HasForeignKey("PostId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Post");
+                });
+
             modelBuilder.Entity("project_garage.Models.DbModels.PostModel", b =>
                 {
                     b.HasOne("project_garage.Models.DbModels.UserModel", "User")
@@ -576,10 +607,6 @@ namespace project_garage.Migrations
                     b.Navigation("Messages");
                 });
 
-            modelBuilder.Entity("project_garage.Models.DbModels.PostModel", b =>
-                {
-                    b.Navigation("Comments");
-                });
 
             modelBuilder.Entity("project_garage.Models.DbModels.UserModel", b =>
                 {
@@ -588,6 +615,8 @@ namespace project_garage.Migrations
                     b.Navigation("Friends");
 
                     b.Navigation("Posts");
+
+                    b.Navigation("Images");
                 });
 #pragma warning restore 612, 618
         }
--- project_garage/Models/ViewModels/EditPostViewModel.cs
+++ project_garage/Models/ViewModels/EditPostViewModel.cs
@@ -5,5 +5,6 @@ namespace project_garage.Models.ViewModels
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public List<string> ImageUrls { get; set; } = new List<string>();
     }
 }
