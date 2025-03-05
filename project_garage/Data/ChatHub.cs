552af1c1f1d4977adcba324cdd5c530f0c6db9eb
Roman <lugovijroman39@gmail.com>
2025-03-06 00:26:45 +0200

    Resolved merge conflict

---
+++ project_garage/Data/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace project_garage.Data
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string conversationId, string senderId, string senderName, string message)
+        {
+            // Транслюємо всім у конкретній кімнаті (групі)
+            await Clients.Group(conversationId).SendAsync("ReceiveMessage", new
+            {
+                SenderId = senderId,
+                SenderName = senderName,
+                Text = message,
+                SendedAt = DateTime.UtcNow
+            });
+        }
+
+        public async Task JoinConversation(string conversationId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, conversationId);
+        }
+
+        public async Task LeaveConversation(string conversationId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, conversationId);
+        }
+    }
+}
