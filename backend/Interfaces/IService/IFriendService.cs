1c87294c45810ad08010ea8d083092f8b3390b26
Roman <159826950+romanl231@users.noreply.github.com>
2025-03-17 13:46:03 +0200

    Backend/refactor/friend controller service repository (#118)
    * Merge fixes
    
    * Added reaction service and repository
    
    * Added reaction controller
    
    * Refactored ProfileController for REST API standarts
    
    * Refactored MessageController for REST API standarts
    
    * Added profile picture
    
    * Added appsettings.json to gitignore
    
    * Optimized db queries at ConversationController
    
    * Resolved merge conflict
    
    * My commit
    
    * Added SignalR for chat at backend api
    
    * Updated migrations
    
    * Addeda authentification for ChatHub
    
    * Added pagination for messages
    
    * Added pagination for conversations
    
    * Added create post
    
    * Added create post page
    
    * Small refactor
    
    * Made the requested changes
    
    * Added user interests (#108)
    
    * Refactor/added dotenv (#113)
    
    * Added user interests
    
    * Added dotenv
    
    * Refactor/renamed backend folder (#114)
    
    * Refactored HomeController for REST API standarts (#106)
    
    * Added user interests
    
    * Added dotenv
    
    * Renamed backend folder
    
    ---------
    
    Co-authored-by: mykola <165220747+tra1lxD@users.noreply.github.com>
    
    * Added constant.js
    
    * Added authentification for frontend
    
    * Refactored Friend controller, service and repository
    
    ---------
    
    Co-authored-by: mykola <165220747+tra1lxD@users.noreply.github.com>

--- backend/Interfaces/IService/IFriendService.cs
+++ backend/Interfaces/IService/IFriendService.cs
@@ -7,10 +7,12 @@ namespace project_garage.Interfaces.IService
         Task<int> GetFriendsCount(string id);
         Task<bool> IsFriendAsync(string userId, string friendId);
         Task<bool> CanAddFriendAsync(string userId, string friendId);
+        Task<List<FriendModel>> GetFriendsAsync(string userId, string? lastFriendId, int limit);
+        Task<List<FriendModel>> GetIncomingRequestsAsync(string userId, string? lastFriendId, int limit);
+        Task<List<FriendModel>> GetOutcomingRequestsAsync(string userId, string? lastFriendId, int limit);
         Task<FriendModel> GetByIdAsync(string id);
-        Task<List<FriendModel>> GetByUserIdAsync(string id);
-        Task<bool> SendFriendRequestAsync(string userId, string friendId);
-        Task AcceptRequestAsync(string userId, string friendId);
-        Task RejectOrDeleteAsync(string userId, string friendId);
+        Task SendFriendRequestAsync(string userId, string friendId);
+        Task AcceptRequestAsync(string requestId);
+        Task RejectOrDeleteAsync(string requestId);
     }
 }
