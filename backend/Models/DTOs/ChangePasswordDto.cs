2407a4017b1eeac912ad86e921a07d1efdaccdb8
Roman <159826950+romanl231@users.noreply.github.com>
2025-03-24 14:27:50 +0200

    Feature/change email (#133)
    * Added reaction service and repository
    
    * Added reaction controller
    
    * Refactored ProfileController for REST API standarts
    
    * Refactored MessageController for REST API standarts
    
    * Added profile picture
    
    * Added appsettings.json to gitignore
    
    * Optimized db queries at ConversationController
    
    * Resolved merge conflict
    
    * My commit
    
    * Added SignalR for chat at backend api
    
    * Updated migrations
    
    * Addeda authentification for ChatHub
    
    * Added pagination for messages
    
    * Added pagination for conversations
    
    * Added create post
    
    * Added create post page
    
    * Small refactor
    
    * Made the requested changes
    
    * Added user interests (#108)
    
    * Refactor/added dotenv (#113)
    
    * Added user interests
    
    * Added dotenv
    
    * Refactor/renamed backend folder (#114)
    
    * Refactored HomeController for REST API standarts (#106)
    
    * Added user interests
    
    * Added dotenv
    
    * Renamed backend folder
    
    ---------
    
    Co-authored-by: mykola <165220747+tra1lxD@users.noreply.github.com>
    
    * Added constant.js
    
    * Added authentification for frontend
    
    * Refactored Friend controller, service and repository
    
    * Fixed error in HomeController
    
    * Added JWT + Refresh Token authorization
    
    * Added example for appsetings.json (#123)
    
    * Fixed infinite loop between login and homepage
    
    * Fixed infinite loop between login and homepage (#124)
    
    * Fixed infinity loop between login and homepage
    
    * Added feature of email verefication via confirmation code
    
    * Issued a registration of services in ServiceExtensions.cs
    
    * Added ability to change email
    
    * Added alerts on change email (#135)
    
    * Feature/change password (#138)
    
    * Added alerts on change email
    
    * Added ability to change password via email confirmation code

---
+++ backend/Models/DTOs/ChangePasswordDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project_garage.Models.DTOs
+{
+    public class ChangePasswordDto
+    {
+        [Required]
+        public string ConfirmationCode { get; set; }
+
+        [Required]
+        public string NewPassword { get; set; }
+    }
+}
