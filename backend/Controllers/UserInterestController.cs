1ea78ca97fea2f339306fc52bead9f1e0efa1be3
Roman <159826950+romanl231@users.noreply.github.com>
2025-04-21 15:29:46 +0300

    Feature/choose interests (#169)
    * Merge fixes
    
    * Chats sorted by last update in real time
    
    * Db updated
    
    * Added InterestRepository and rebuilded UserInterestController logic
    
    * Added Middleware to Fill Interests Table
    
    * Added UI for choosing interests

--- backend/Controllers/UserInterestController.cs
+++ backend/Controllers/UserInterestController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using project_garage.Data;
 using project_garage.Interfaces.IService;
 
 namespace project_garage.Controllers
 {
     [Route("api/interest")]
+    [Authorize]
     public class UserInterestController : ControllerBase
     {
         private readonly IUserInterestService _userInterestService;
@@ -14,11 +16,12 @@ namespace project_garage.Controllers
             _userInterestService = userInterestService;
         }
 
-        [HttpGet("{userId}")]
-        public async Task<IActionResult> GetUserInterest(string userId)
+        [HttpGet("my")]
+        public async Task<IActionResult> GetUserInterest()
         {
             try
             {
+                var userId = UserHelper.GetCurrentUserId(HttpContext);
                 var interests = await _userInterestService.GetUserInterestAsync(userId);
                 return Ok(interests);
             }
@@ -33,12 +36,12 @@ namespace project_garage.Controllers
         }
 
         [HttpPost("add")]
-        public async Task<IActionResult> AddUserInterestForCurrentUser(List<string> interests)
+        public async Task<IActionResult> AddUserInterestForCurrentUser([FromBody]List<int> interestIds)
         {
             try
             {
                 var userId = UserHelper.GetCurrentUserId(HttpContext);
-                await _userInterestService.AddInterestAsync(userId, interests);
+                await _userInterestService.AddInterestAsync(userId, interestIds);
                 return Ok("User interest successfully added");
             }
             catch (ArgumentException ex)
@@ -52,11 +55,12 @@ namespace project_garage.Controllers
         }
 
         [HttpDelete("remove/{interestId}")]
-        public async Task<IActionResult> RemoveUserInterest(string interestId)
+        public async Task<IActionResult> RemoveUserInterest(int interestId)
         {
             try
             {
-                await _userInterestService.RemoveInterestAsync(interestId);
+                var userId = UserHelper.GetCurrentUserId(HttpContext);
+                await _userInterestService.RemoveInterestAsync(interestId, userId);
                 return Ok("Interest successfully removed");
             }
             catch (ArgumentException ex)
---
+++ backend/Middleware/EnsureInterestsMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using project_garage.Bogus;
+
+namespace project_garage.Middleware
+{
+    public class EnsureInterestsMiddleware
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly RequestDelegate _next;
+
+        public EnsureInterestsMiddleware(
+            IConfiguration configuration, 
+            IServiceProvider serviceProvider, 
+            RequestDelegate requestDelegate) 
+        {
+            _configuration = configuration;
+            _serviceProvider = serviceProvider;
+            _next = requestDelegate;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            DbSeeder.EnsureInterestsConsistent(_serviceProvider, _configuration);
+            await _next(context);
+        }
+    }
+}
